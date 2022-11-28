c63d1f116c809bb778c8131003d20506e1d22837
pegaser <sperezgabriel@gmail.com>
2022-11-28 14:16:18 -0600

    Agregar archivos de proyecto.

---
+++ Maui.Introduction/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.Introduction;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Maui.Introduction/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Maui.Introduction;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
